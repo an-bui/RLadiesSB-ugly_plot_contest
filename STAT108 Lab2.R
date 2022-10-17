b0f3bdd329c7f66e1ff45ed349bdfb0bfd900cf3
ysun155 <ysun155@ucsc.edu>
2022-10-17 13:05:43 -0700

    Done with lab2!

--- STAT108 Lab2.R
+++ STAT108 Lab2.R
@@ -8,14 +8,14 @@ data <- read_csv("data/use_this_data/holiday_sweaters-2020-12-15-clean.csv") %>%
   rownames_to_column("values")
   
 ggplot(data = data, 
-       mapping = aes(x = colors, y = sum)) + 
-  geom_point(size = 20, alpha = 1/10) + 
+       mapping = aes(x = colors, y = sum, color = factor(lights))) + 
+  geom_point(size = 20, alpha = 1/10, aes(color = factor(lights))) + 
   theme(
     plot.background = element_rect(fill = "red"),
     panel.background = element_rect(fill = 'lightblue', colour = 'green', size = 4),
     axis.title.x = element_text(face = "bold.italic", color = "blue"),
     axis.title.y = element_text(family = "mono", face = "bold", color = "blue"),
-    legend.background = element_rect(fill = "orangered4")
+    legend.background = element_rect(fill = "blue")
     ) +
   labs(x = "colors",
        y = "total number of colors in the sweater")
